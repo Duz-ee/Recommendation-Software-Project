3b566958772ed27b4763de36ec54e6c12b216831
Duz-ee <nchiduziem@gmail.com>
2022-07-29 08:27:07 -0400

    Tentative data on bars in windsor

---
+++ Windsor_Bars_Data.py
@@ -0,0 +1,10 @@
+legend = [Name, Google_rating, live_music, Address]
+
+bars = [["Lefty's on the O", "4.2","Yes", "341 Ouellete Ave"], ["The Patio Lounge Windsor", "3.7", "No" "507 Ouellete Ave"], 
+["The Loose Goose RestoPub & Lounge", "4.4", "No", "126 Ouellete Ave"], ["Treehouse Bar & Grill", 4.2, "351 Ouellete Ave"], 
+["Phog Lounge", 4.6, "157 University Ave W"], ["The Dugout Sports Lounge", 4.2, "157 University Ave W"], 
+["The Liquid Library", 4.5, "2135 Wyandotte St W"], ["The Manchester Pub", 4.4, "546 Ouellete Ave"],
+["Legends Sports Bar", 3.9, "377 Riverside Dr E"], ["The Bull & Barrel Urban Saloon", 3.9, "670 Ouellette Ave"],
+["Maiden Lane Wine & Spirits", 4.8, "494 Pelissier St"], ["Rockhead Pub", 4.2, "1444 Ottawa St"],
+["TQLA", 3.0, "481 Ouellete Ave"], ["Eastwood's Grill and Lounge", 4.5, "63 Riverside Dr E"]
+["Vu Bar", 4.3, "377 Riverside Dr E"], ["Jimmy G's Bar and Grill", ]]
